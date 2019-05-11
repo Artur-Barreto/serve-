bc24383011cae2a3b537f6425948b234eab8a50f
Artur Barreto <50060553+Artur-Barreto@users.noreply.github.com>
2019-05-11 14:26:47 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,33 @@
-const http = require('http')
+const Koa = require ('koa')
+const Router = require('koa-router')
 
-const server = http.createServer((req, res) => {
-    const resposta = []
-    resposta['/'] = '<h1>Home</h1>'
-    resposta['/contato'] = '<h1>Contato</h1>'
-    resposta['semURL'] = '<h1>URL sem reposta definida!</h1>'
+const server = new Koa()
+const router = new Router()
 
-    res.end(resposta[res.url] || resposta['SemURL'])
+router
+.get('/', (ctx) => {
+    ctx.body = '<h1> Você tá na Home!!</h1>'
 })
+.get('/contato', (ctx) => {
+    ctx.body = `
+        <h1>Contato</h1>
+        
+            <form action="/contato method="POST">
+            <label for="email">Email:</label>
+            <input type="email" name"email" id="email">
 
-server.listen(3001, 'localhost', () => {
-    console.log('Servidor de pé em: http://localhost:3001')
-    console.log('Pra desligar o servidor: Ctrl + c')
+            <label for="mensagem">Mensagem:</label>    
+            <textarea name="mensagem" id="mensagem"></textarea>
 
+            <input type="submit" value="Enviar">
+            </form>
+        `
+})
+.ṕost('/contato')
+
+server.use(router.routes())
+
+server.listen(3001, () => {
+    console.log('Servidor de pé em http://localhost:3001')
+    console.log('Para derrubar o servidor: ctrl + c')
 })
